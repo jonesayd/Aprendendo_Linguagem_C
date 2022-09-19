9c1d01e50377be290bf15c2261841f0055b92d26
Jone Sayd <112839474+jonesayd@users.noreply.github.com>
2022-09-19 12:27:22 -0400

    add hello world

---
+++ Hello_world.c
@@ -0,0 +1,14 @@
+//O comando #include serve para declarar as bilbiotecas que serão utilizadas
+#include <stdio.h> //O 'stdio.h' contém as funções de leitura do teclado e escrita em tela
+#include <stdlib.h>
+
+//Início do Programa
+//Muitos programas começam com a função 'void main', mas como este não retorna nenhum valor a mais apropriada é a "main" como inteira (int main), usada abaixo, pois o seu valor poderá indicar se o programa foi finalizado corretamente ou não.
+int main () {
+
+//Escreve uma mensagem na tela de saída. Obs.: Sempre entre aspas duplas e não esquecer ao final de colocar ponto e vírgula:
+    printf("Hello, World!"); 
+
+    return 0;
+
+}
